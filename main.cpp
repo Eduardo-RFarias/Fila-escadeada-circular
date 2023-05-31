ed5a59f6d15a7c33b52258d442d97729cb0f85b1
Eduardo-RFarias <eduardo.rfarias@outlook.com>
2023-05-31 02:30:00 -0300

    comentários

--- main.cpp
+++ main.cpp
@@ -26,6 +26,7 @@ int main()
         cin >> process.id;
         cin >> process.timeLeft;
 
+        // Convert to milliseconds
         process.timeLeft *= 1000;
 
         list.add(process);
@@ -33,15 +34,23 @@ int main()
 
     int time = 0;
 
+    // Iterate over the queue until it's empty
     for (auto node = list.next(); !list.isEmpty(); node = list.next())
     {
+        // Get the process from the node
         Process *process = &node->data;
 
+        // Subtract the quantum from the process time and add it to the total time
         process->timeLeft -= quantum;
         time += quantum;
 
+        // If after subtracting the quantum the process time is less than or equal to 0
+        // then the process is done and we can print it
         if (process->timeLeft <= 0)
         {
+            // Maybe the process took less than the quantum to finish
+            // so we need to add the remaining time to the total time
+            // in this case the remaining time is the negative value of the process time
             time += process->timeLeft;
             cout << process->id << " (" << time << ")" << endl;
             list.deleteCurrent();
